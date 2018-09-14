2434ecc75ac19bdd19acca6df6a79b814b459d15
leo <wuzhenglin510@163.com>
2018-09-14 18:14:44 +0800

    fix

--- page/popup/popup.js
+++ page/popup/popup.js
@@ -11,9 +11,20 @@ $(function () {
             chrome.tabs.executeScript(
                 tabs[0].id, {
                     code: `
-                        var teemo_inject_script = document.createElement('script');
-                        teemo_inject_script.src = 'http://localhost:6385/teemo_inject_js.js'
-                        document.body.appendChild(teemo_inject_script);
+                        var teemo_inject_xpath = document.createElement('script');
+                        teemo_inject_xpath.src = 'http://localhost:6385/teemo_inject_xpath.js'
+                        document.body.appendChild(teemo_inject_xpath);
+
+                        var teemo_inject_common_script = document.createElement('script');
+                        teemo_inject_common_script.src = 'http://localhost:6385/teemo_inject_common_js.js'
+                        document.body.appendChild(teemo_inject_common_script);
+
+                        setTimeout(function () {
+                            var teemo_inject_group_script = document.createElement('script');
+                            teemo_inject_group_script.src = 'http://localhost:6385/teemo_inject_group_js.js'
+                            document.body.appendChild(teemo_inject_group_script);
+                        }, 500)
+                        
                         var teemo_inject_link = document.createElement('link');
                         teemo_inject_link.type = "text/css";
                         teemo_inject_link.rel = "stylesheet";
